8be1c00adc875d5f909ab99c55187776bba23703
Ubuntu <aureliop@DEVWS2.qwjjfcpquhee3lq4m1dzewvucc.ax.internal.cloudapp.net>
2015-11-12 12:23:38 +0000

    initial version

---
+++ HelloWorldJS/app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name helloWorldJsApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the helloWorldJsApp
+ */
+angular.module('helloWorldJsApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ HelloWorldJS/app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name helloWorldJsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the helloWorldJsApp
+ */
+angular.module('helloWorldJsApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
